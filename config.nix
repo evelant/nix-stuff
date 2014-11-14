5fc328328701ecea071c80a1fda2434212a0cdcd
Andrew Morsillo <andrew@peeramid.org>
2014-11-14 14:46:30 -0500

    update

--- config.nix
+++ config.nix
@@ -1,9 +1,11 @@
 {
+  allowUnfree = true;
   vim.ruby = true;
+  allowBroken = true;
   packageOverrides = pkgs : with pkgs; {
     rvmEnv = pkgs.myEnvFun {
       name = "rvm";
-      buildInputs = [ stdenv which readline zlib openssl libxml2 libxslt libiconv ];
+      buildInputs = [ stdenv  python pythonPackages.boto procps which libyaml readline zlib openssl libxml2 libxslt libiconv ];
       extraCmds = ''
         unset http_proxy
         unset ftp_proxy
@@ -12,9 +14,15 @@
         export LIBICONV_DIR=${libiconv}
         export OPENSSL_DIR=${openssl}
         export READLINE_DIR=${readline}
-        bash --login
-        rvm use ruby
+        export LIBYAML_DIR=${libyaml}
+        export LOCAL_REDIS=true
+        #bash --login
+        . ~/.rvm/scripts/rvm
+        #rvm use ruby
       '';
     };
   };
 }
+# to install fucking nokogiri
+#gem install nokogiri -- --use-system-libraries --with-xml2-include=$LIBXML2_DIR/include/libxml2 --with-xml2-lib=$LIBXML2_DIR/lib --with-xslt-dir=$LIBXSLT_DIR --with-iconv-include=$LIBICONV_DIR/include --with-iconv-lib=$LIBICONV_DIR/lib
+
